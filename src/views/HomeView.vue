b781ebddd2758e6e38fcdd036bdf68904711e099
Ignacio Malfavon Gutierrez <ignacio_96@live.com.mx>
2024-04-16 01:02:00 -0600

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
+  <div class="home container">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hola Mundo</h1>
+    <button class=" ms-1 btn btn-success">1 Success</button>
+    <button class=" ms-1 btn btn-primary">2 Primary</button>
+    <button class=" ms-1 btn btn-secondary">3 Secondary</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
